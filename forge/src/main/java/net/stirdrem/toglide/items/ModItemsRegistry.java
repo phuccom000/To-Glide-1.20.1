e8cc31bdcc55f72777e7a386d84938980cb33a47
phuccom000 <gdtitans6@gmail.com>
2026-04-12 10:42:10 +0700

    removed other items

--- forge/src/main/java/net/stirdrem/toglide/items/ModItemsRegistry.java
+++ forge/src/main/java/net/stirdrem/toglide/items/ModItemsRegistry.java
@@ -21,22 +21,6 @@ public class ModItemsRegistry {
     public static final RegistryObject<Item> WOODEN_GLIDER = ITEMS.register("wooden_glider",
             () -> new ForgeGliderItem(-0.12, 1.06, new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> STONE_GLIDER = ITEMS.register("stone_glider",
-            () -> new ForgeGliderItem(-0.22, 1.02, new Item.Properties().stacksTo(1)));
-
-    public static final RegistryObject<Item> IRON_GLIDER = ITEMS.register("iron_glider",
-            () -> new ForgeGliderItem(-0.18, 1.03, new Item.Properties().stacksTo(1)));
-
-    public static final RegistryObject<Item> GOLDEN_GLIDER = ITEMS.register("golden_glider",
-            () -> new ForgeGliderItem(-0.16, 1.04, new Item.Properties().stacksTo(1)));
-
-    public static final RegistryObject<Item> DIAMOND_GLIDER = ITEMS.register("diamond_glider",
-            () -> new ForgeGliderItem(-0.14, 1.05, new Item.Properties().stacksTo(1)));
-
-    public static final RegistryObject<Item> NETHERITE_GLIDER = ITEMS.register("netherite_glider",
-            () -> new ForgeGliderItem(-0.12, 1.06,
-                    new Item.Properties().stacksTo(1).fireResistant()));
-
     private static void addItemsToCreativeTabs(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(WOODEN_GLIDER.get());
